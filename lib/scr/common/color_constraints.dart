c579402dcf88cbb33e5d9e977b5fcf8e91da87f0
abaydevios <90930322+abaydevios@users.noreply.github.com>
2022-09-03 15:36:38 +0600

    colors_constraints_commit

---
+++ lib/scr/common/color_constraints.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static const Color scaffoldBackground = Color(0xFFF3F4F6);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color main = Color.fromARGB(255, 26, 145, 64);
+}
+
+class AppPaddings {
+  static const EdgeInsets horizontal = EdgeInsets.symmetric(horizontal: 16);
+}
